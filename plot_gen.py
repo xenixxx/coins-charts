69dd2b3fbfebd7504e946727ac33feed44f4de72
xenix <xenix@tuxy.eu>
2021-01-15 14:40:26 +0100

    charts for conky

--- plot_gen.py
+++ plot_gen.py
@@ -17,4 +17,13 @@ idf.set_index('minute', inplace=True)
 save = dict(fname='t30.png', dpi=100, pad_inches=0.25, transparent=True)
 mpf.plot(idf, type='candle', style='checkers', mav=(3, 6, 9), ylabel='BTC / USD', savefig=save)
 
+saveTmp = dict(fname='/tmp/btc_chart.png', dpi=90, pad_inches=0.25, transparent=True)
+mpf.plot(idf, type='candle', style='mike', ylabel='BTC / USD', savefig=saveTmp)
+
+idf = pd.read_csv("bitcoin_data_tut.csv")
+idf['minute'] = pd.to_datetime(idf['minute'], format="%m/%d/%Y %H:%M")
+idf.set_index('minute', inplace=True)
+saveTmp = dict(fname='/tmp/btc_chart_day.png', dpi=90, pad_inches=0.25, transparent=False)
+mpf.plot(idf, type='line', style='mike', mav=(3, 6, 9), ylabel='BTC / USD', savefig=saveTmp)
+
 # genPlot()
