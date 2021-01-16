1f8426a9e8e2b34bbe5387f1a0f5ffe91cd4ff51
dev0 <xenix@wp.pl>
2021-01-16 11:12:33 +0100

    Initial commit

--- plot_gen.py
+++ plot_gen.py
@@ -11,21 +11,24 @@ from datetime import date, datetime
 
 # def genPlot():
 #    threading.Timer(10.0, genPlot).start()
-idf = pd.read_csv("/home/user/PycharmProjects/coins-charts/bitcoin_data_tut.csv")
+fcsv = 'bitcoin_data_tut.csv'
+ftail = 60
+
+# /home/user/PycharmProjects/coins-charts/
+idf = pd.read_csv(fcsv)
 idf['minute'] = pd.to_datetime(idf['minute'], format="%m/%d/%Y %H:%M")
 idf.set_index('minute', inplace=True)
 save = dict(fname='t30.png', dpi=100, pad_inches=0.25, transparent=True)
 mpf.plot(idf, type='candle', style='checkers', mav=(3, 6, 9), ylabel='BTC / USD', savefig=save)
 
 saveTmp = dict(fname='/tmp/btc_chart.png', dpi=90, pad_inches=0.25, transparent=True)
-mpf.plot(idf.tail(60), type='candle', style='mike', ylabel='BTC / USD', savefig=saveTmp)
+mpf.plot(idf.tail(ftail), type='candle', style='mike', ylabel='BTC / USD', savefig=saveTmp)
 
 
-idf = pd.read_csv("/home/user/PycharmProjects/coins-charts/bitcoin_data_tut.csv")
-# idf = pd.read_csv("", index_col=100)
+idf = pd.read_csv(fcsv)
 idf['minute'] = pd.to_datetime(idf['minute'], format="%m/%d/%Y %H:%M")
 idf.set_index('minute', inplace=True)
-saveTmp = dict(fname='/tmp/btc_chart_day.png', dpi=90, pad_inches=0.25, transparent=False)
+saveTmp = dict(fname='/tmp/btc_chart_day.png', dpi=90, pad_inches=0.25, transparent=True)
 mpf.plot(idf, type='line', style='mike', mav=(3, 6, 9), ylabel='BTC / USD', savefig=saveTmp)
 
 # genPlot()
